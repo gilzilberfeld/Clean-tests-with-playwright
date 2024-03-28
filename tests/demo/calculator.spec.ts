753968f6f39f12ce6534329037e1e90a0de4483c
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-03-28 14:55:38 +0200

    comments

--- tests/demo/calculator.spec.ts
+++ tests/demo/calculator.spec.ts
@@ -1,11 +1,14 @@
 import { expect, test } from "@playwright/test";
 
 test("calculator test", async ({ page }) => {
+  // navigation  
   await page.goto("/calculator");
+
+  // locators and operations
   await page.getByRole("textbox", { name: "First" }).fill("1");
   await page.getByRole("textbox", { name: "Second" }).fill("2");
-
   await page.getByRole("button", { name: "Calculate" }).click();
 
+  // after navigation checking the text is there
   await expect(page.getByText("And the result is.... 3")).toBeVisible();
 });
